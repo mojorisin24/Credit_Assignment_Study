202ed066594a391064e05530980001223893ecaf
Carlos Carrasco <cdcarrasco@ucdavis.edu>
2020-05-03 21:29:09 -0700

    changed fillout

--- html/Behavioral_MBCA-legacy-browsers.js
+++ html/Behavioral_MBCA-legacy-browsers.js
@@ -17,7 +17,7 @@ psychoJS.openWindow({
 
 // store info about the experiment session:
 let expName = 'Behavioral_MBCA';  // from the Builder filename that created this script
-let expInfo = {'participant': '', 'session': '001'};
+let expInfo = {'participant': '', 'age': '', 'biological sex (m/f/i)': '', 'handedness (l/r)': ''};
 
 // schedule the experiment:
 psychoJS.schedule(psychoJS.gui.DlgFromDict({
